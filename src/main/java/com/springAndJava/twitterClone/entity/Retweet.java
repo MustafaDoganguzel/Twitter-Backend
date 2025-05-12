884107acb9e1ad191056c314fe9073411818db7a
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-05-13 00:36:06 +0300

    Add Retweet entity with relationships to other entities

---
+++ src/main/java/com/springAndJava/twitterClone/entity/Retweet.java
@@ -0,0 +1,25 @@
+package com.springAndJava.twitterClone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "retweet" , schema= "public")
+public class Retweet {
+    private Long id;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "tweet_id")
+    private Tweet tweet;
+}
