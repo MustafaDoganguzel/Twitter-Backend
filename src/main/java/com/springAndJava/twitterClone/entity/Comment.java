04e37d758e25badcde1f4b944a50f615a65c5293
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-05-13 00:35:53 +0300

    Add Comment entity with relationships to other entities

---
+++ src/main/java/com/springAndJava/twitterClone/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.springAndJava.twitterClone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "comment" , schema= "public")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "tweet_id")
+    private Tweet tweet;
+
+}
