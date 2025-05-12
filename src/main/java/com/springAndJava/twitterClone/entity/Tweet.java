cfafb0ae1861c90fa265ae5dc22bc353cdb2c32b
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-05-13 00:35:27 +0300

    Add Tweet entity with relationships to entities

---
+++ src/main/java/com/springAndJava/twitterClone/entity/Tweet.java
@@ -0,0 +1,37 @@
+package com.springAndJava.twitterClone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tweet" , schema= "public")
+public class Tweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tweet")
+    private List<Like> likes = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tweet")
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tweet")
+    private List<Retweet>retweets = new ArrayList<>();
+
+
+}
