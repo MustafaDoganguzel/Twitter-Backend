bcdb3413497f80bd1f18d40cafa6c4c44038fe06
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-05-13 16:30:35 +0300

    Implement AuthController with register endpoint

---
+++ src/main/java/com/springAndJava/twitterClone/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.springAndJava.twitterClone.controller;
+
+import com.springAndJava.twitterClone.entity.User;
+import com.springAndJava.twitterClone.service.AuthenticationService;
+import dto.RegistrationUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class AuthController {
+    private AuthenticationService authenticationService;
+    @Autowired
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody RegistrationUser registrationUser){
+        return authenticationService.register(registrationUser.username(),
+                registrationUser.email(), registrationUser.password());
+
+    }
+}
